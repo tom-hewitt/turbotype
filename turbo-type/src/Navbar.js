1f8aa3143ccb7d9e94f48003fd941b8f1a8f5496
z07151th <thomas.hewitt-3@student.manchester.ac.uk>
2022-12-14 17:02:09 +0000

    Make navbar declarative

--- turbo-type/src/Navbar.js
+++ turbo-type/src/Navbar.js
@@ -1,33 +1,52 @@
 import logo from './logo.svg';
 import profile_icon from './profile_icon.svg';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
-    const openCollapsedMenu = () => {
-        const navigationList = document.getElementsByClassName('navbar-item');
-        for (let i = 0; i < navigationList.length; i++) {
-            navigationList[i].classList.toggle('btn-active');
-        }
-        const pageContent = document.getElementsByClassName('content');
-        pageContent[0].classList.toggle('hide-content');
-    }
+    const [showCollapsedMenu, setShowCollapsedMenu] = useState(false);
+
+    const toggleCollapsedMenu = () => setShowCollapsedMenu(!showCollapsedMenu);
 
     return (
-        <nav className="navbar">   
-            <Link to="/"><img src={ logo } id="logo" alt="logo" /></Link>   
-            <div className="links">  
-                <Link className="navbar-item" to="/play">PLAY</Link>
-                <Link className="navbar-item" to="/tournaments">TOURNAMENTS</Link>
-                <Link className="navbar-item" to="/customise">CUSTOMISE</Link>
-                <Link id="profile" to="/profile"><img src={ profile_icon } id="profile-icon" alt="profile icon" />
-                <div id="profile-name">Adam H</div></Link>
-            </div>
-            <div className='collapsed-menu' onClick={ openCollapsedMenu }>
-                <div className='menu-line'></div>
-                <div className='menu-line'></div>
-                <div className='menu-line'></div>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar">
+                <Link to="/">
+                    <img src={logo} id="logo" alt="logo" />
+                </Link>
+                <div className="links">
+                    <Link to="/play">PLAY</Link>
+                    <Link to="/tournaments">TOURNAMENTS</Link>
+                    <Link to="/customise">CUSTOMISE</Link>
+                    <Link id="profile" to="/profile">
+                        <img src={ profile_icon } id="profile-icon" alt="profile icon" />
+                        <div id="profile-name">Adam H</div>
+                    </Link>
+                </div>
+                <CollapsedMenuButton onClick={toggleCollapsedMenu} />
+            </nav>
+            {showCollapsedMenu ? 
+                <div className="collapsed-menu">
+                    <Link to="/play">PLAY</Link>
+                    <Link to="/tournaments">TOURNAMENTS</Link>
+                    <Link to="/customise">CUSTOMISE</Link>
+                    <Link id="profile" to="/profile">
+                        <img src={ profile_icon } id="profile-icon" alt="profile icon" />
+                        <div id="profile-name">Adam H</div>
+                    </Link>
+                </div>
+            : null}
+        </>
+    );
+}
+
+const CollapsedMenuButton = (props) => {
+    return (
+        <button className='collapsed-menu-button' onClick={props.onClick}>
+            <div className='menu-line'></div>
+            <div className='menu-line'></div>
+            <div className='menu-line'></div>
+        </button>
     );
 }
  

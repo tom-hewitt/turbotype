abf29631d0e02475c3ebe4db54d9caef9290dfc5
Thomas Hewitt <thomas.hewitt-3@student.manchester.ac.uk>
2023-01-29 19:25:12 +0000

    simplify function

--- game-server/src/check-the-input/check-input.tsx
+++ game-server/src/check-the-input/check-input.tsx
@@ -4,16 +4,8 @@
  * new word will be generated from where the function is called
  * called at each key press event
  */ 
- function checkInput(key: string, word:string, index:number): boolean {
-
-    if (key === word[index]) {
-        return true;
-        // will need to increase index from where we call this function
-    }
-    else {
-        return false;
-    }
-
+function checkInput(key: string, word: string, index: number): boolean {
+    return key === word[index];
 }
 
 

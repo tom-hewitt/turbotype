3b62941913136563483e346c568b52cb370cc344
j40432jj <jiamin.jiang@student.manchester.ac.uk>
2023-01-29 21:33:54 +0000

    changed so that the parameter time_elapsed is in seconds

--- game-server/src/calculate-wpm/calculate-wpm.ts
+++ game-server/src/calculate-wpm/calculate-wpm.ts
@@ -1,11 +1,11 @@
 /**
  * 
- * @param time_elapsed calcuated from when the race starts
+ * @param time_elapsed time in seconds calculated from when the race starts
  * @param chars_num this increases every time checkInput returns true
  * @returns wpm
  */
 
 export function calculateWPM(time_elapsed: number, chars_num: number):number{
 
-    return Math.round((chars_num/5)/time_elapsed);
+    return Math.round((chars_num/5)/(time_elapsed/60));
 }

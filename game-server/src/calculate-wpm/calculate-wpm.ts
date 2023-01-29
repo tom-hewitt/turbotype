854d459eac44cdb07f42d8317fa45b26aa22ff49
j40432jj <jiamin.jiang@student.manchester.ac.uk>
2023-01-29 20:58:37 +0000

    calculate wpm

---
+++ game-server/src/calculate-wpm/calculate-wpm.ts
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param time_elapsed calcuated from when the race starts
+ * @param chars_num this increases every time checkInput returns true
+ * @returns wpm
+ */
+
+export function calculateWPM(time_elapsed: number, chars_num: number):number{
+
+    return Math.round((chars_num/5)/time_elapsed);
+}

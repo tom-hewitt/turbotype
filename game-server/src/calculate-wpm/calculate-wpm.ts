6a591ea99ccdc25eda06283318449e26c3b16b09
z07151th <thomas.hewitt-3@student.manchester.ac.uk>
2023-02-01 00:32:33 +0000

    wpm fix

--- game-server/src/calculate-wpm/calculate-wpm.ts
+++ game-server/src/calculate-wpm/calculate-wpm.ts
@@ -1,12 +1,15 @@
 /**
- * 
+ *
  * @param seconds_elapsed time in seconds calculated from when the race starts
  * @param character_count the number of characters typed over the given time period
  * @returns wpm
  */
-export function calculate_WPM(seconds_elapsed: number, character_count: number): number {
-    const word_count = chars_num / 5;
-    const minutes_elapsed = seconds_elapsed / 60;
-    
-    return word_count / minutes_elapsed;
+export function calculate_WPM(
+  seconds_elapsed: number,
+  character_count: number
+): number {
+  const word_count = character_count / 5;
+  const minutes_elapsed = seconds_elapsed / 60;
+
+  return word_count / minutes_elapsed;
 }

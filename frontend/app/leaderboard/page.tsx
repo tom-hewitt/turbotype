6eb64c339f1218598ab56265a9b5535631a47908
p23924kc <ka.chiu@student.manchester.ac.uk>
2023-02-15 10:15:26 +0000

    Create a leaderboard branch and add somethings on the page

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -5,6 +5,15 @@ export default function Leaderboard() {
   return (
     <Page colour="#FFECE4">
       <Heading>LEADERBOARD</Heading>
+      <nav>
+        <ul style={{ display: 'flex', listStyleType: 'none', padding: '0.5rem', fontSize: '1.0rem' }}>
+          <li style={{ marginRight: '1rem' }}><a href="#">ALL TIME</a></li>
+          <li style={{ marginRight: '1rem' }}><a href="#">YEAR</a></li>
+          <li style={{ marginRight: '1rem' }}><a href="#">MONTH</a></li>
+          <li style={{ marginRight: '1rem' }}><a href="#">WEEK</a></li>
+          <li style={{ marginRight: '1rem' }}><a href="#">DAY</a></li>
+        </ul>
+      </nav>
     </Page>
   );
 }

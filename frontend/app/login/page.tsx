a71611c99e9c586f73a105167b36e7d21b463f90
p23924kc <ka.chiu@student.manchester.ac.uk>
2023-03-06 17:20:01 +0000

    Create the leaderboard page and profile page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { Heading } from "../../components/heading";
 import { Page } from "../../components/page";
 
-export default function Leaderboard() {
+export default function Login() {
     return (
       <Page colour="#FFECE4">
         <Heading>LOGIN</Heading>

3522a0e5bc0681d01c36fc5d5d27098f841f61bd
p23924kc <ka.chiu@student.manchester.ac.uk>
2023-03-23 13:04:56 +0000

    use the color picker to change the color of the car

--- frontend/app/customise/page.tsx
+++ frontend/app/customise/page.tsx
@@ -1,10 +1,15 @@
 import { Heading } from "../../components/heading";
 import { Page } from "../../components/page";
+import { Subheading } from "../../components/subheading";
+import React from 'react';
+import { Editor } from "./editor";
 
 export default function Customise() {
   return (
     <Page colour="#FFABB0">
       <Heading>CUSTOMISE</Heading>
+      <Subheading>Let's decorate your own car.</Subheading>
+      <Editor />
     </Page>
   );
 }

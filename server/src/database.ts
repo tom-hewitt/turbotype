014d9c2dbb7be6f4d1b342348e852750b2a2d271
z07151th <thomas.hewitt-3@student.manchester.ac.uk>
2023-03-24 17:46:36 +0000

    fix supabase error

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@turbotype/database";
 
-const supabase = createClient<Database>(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
-
 export const getColorFromDatabase = async (id: string): Promise<string> => {
+  const supabase = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+
   const res = await supabase
     .from("users")
     .select("color")
@@ -25,6 +25,11 @@ export const addSummaryToDatabase = async (
   race_user_ids: any,
   race_times: any
 ): Promise<string> => {
+  const supabase = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+
   console.log(
     "adding summary to database:\n\tchars:",
     race_chars,
